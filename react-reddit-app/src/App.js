da022611a5aa20d70b93bb1513242a2a3cb10b06
Deserlo <evamarienajera@csus.edu>
2021-01-06 00:47:44 -0800

    removed commented out code

--- react-reddit-app/src/App.js
+++ react-reddit-app/src/App.js
@@ -2,38 +2,6 @@ import './App.css';
 import Main from './components/Main';
 import './main.css';
 
-/*
-function List({ items, fallback }) {
-  if (!items || items.length === 0) {
-    return fallback;
-  } else {
-    //console.log("type of ", typeof(items.data[0]));
-    //console.log(items.data[0]);
-    return items.map(item => {
-      return <div key={item.post_id}><img src={item.post_thumbnail}/>{item.post_title}</div>;
-    });
-  }
-}
-
-const getItems = () => fetch('/home').then(res => res.json());
-
-function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    getItems().then(data => setItems(data));
-  }, []);
-
-  return (
-
-    <div>
-      <Main/>
-      <List items={items} fallback={"Loading..."} />
-    </div>
-  );
-}
-
-*/
 
 function App(){
   return(
