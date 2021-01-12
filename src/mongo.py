949dc92648aa9c2508fc7b131aae745d4ac2af6e
Deserlo <evamarienajera@csus.edu>
2021-01-11 18:29:08 -0800

    added filter queries

--- src/mongo.py
+++ src/mongo.py
@@ -5,32 +5,30 @@ import datetime
 from decouple import config
 import pprint
 
+page_size = 6
+
 
 class Mongo(object):
-    
 
     def __init__(self, mongo_uri):
         self.connection_string = mongo_uri
         self.client = MongoClient(self.connection_string).Reddit.ProgressPics
-        
-   
+
     def insert_many(self, list):
         '''
-        '''      
+        '''
         self.client.insert_many(list)
-
 
     def insert_one(self, doc):
         '''
         '''
         self.client.insert_one(doc)
 
-    
     def delete_many(self):
         '''
         '''
         count = self.client.delete_many({})
-        print (count.deleted_count, " documents deleted.")
+        print(count.deleted_count, " documents deleted.")
 
     def update_one(self, query):
         '''
@@ -40,18 +38,19 @@ class Mongo(object):
     def find(self):
         '''
         '''
-        cursor = self.client.find({})
+        cursor = self.client.find({}).limit(page_size)
         list_cur = list(cursor)
         json_docs = dumps(list_cur)
         return json_docs
 
     def find_one(self):
-        json_doc = dumps(self.client.find_one({"_id": ObjectId("5fefa122384d09c315b12eed")}))
-        #pprint.pprint(self.client.find_one({"_id": ObjectId("5fefa122384d09c315b12eed")}))
-        #json_doc = dumps(list_cur)
+        json_doc = dumps(self.client.find_one(
+            {"_id": ObjectId("5fefa122384d09c315b12eed")}))
+        # pprint.pprint(self.client.find_one({"_id": ObjectId("5fefa122384d09c315b12eed")}))
+        # json_doc = dumps(list_cur)
         return json_doc
 
-    def find_some(self, query):
+    def filter(self, query):
         '''
         '''
         cursor = self.client.find(query)
@@ -59,11 +58,15 @@ class Mongo(object):
         json_docs = dumps(list_cur)
         return json_docs
 
-    
+    def page(self, last_id):
+        cursor = self.client.find({'_id' > last_id}).limit(page_size)
+        list_cur = list(cursor)
+        json_docs = dumps(list_cur)
+        return json_docs
 
 
-
-        
-
-        
-
+'''
+query = {"gender": "F", "age": {"$gt": '40'}}
+MongoDB = Mongo(config('MONGO_URI'))
+print(MongoDB.filter(query))
+'''
